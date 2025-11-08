9a20e5d22534fafe5ee47b65f7e23a3478e961e7
みゆ <rinny.uwu.owo@gmail.com>
2025-11-09 01:08:26 +0900

    Release: NekoList v1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "hanamuramiyu.pawkin"
-version = "1.0.0"
+version = "1.1.0"
 description = "NekoList"
 
 java {
@@ -15,14 +15,18 @@ java {
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.velocitypowered.com/snapshots/")
     maven("https://oss.sonatype.org/content/groups/public/")
     maven("https://m2.dv8tion.net/releases")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
+    compileOnly("com.velocitypowered:velocity-api:3.3.0-SNAPSHOT")
+    annotationProcessor("com.velocitypowered:velocity-api:3.3.0-SNAPSHOT")
     compileOnly("net.luckperms:api:5.4")
     implementation("net.dv8tion:JDA:5.0.0-beta.20")
+    implementation("org.yaml:snakeyaml:2.2")
 }
 
 tasks {
@@ -46,6 +50,11 @@ tasks {
     shadowJar {
         archiveFileName.set("${project.name}-${project.version}.jar")
         minimize()
+        
+        dependencies {
+            exclude(dependency("io.papermc.paper:paper-api:.*"))
+            exclude(dependency("com.velocitypowered:velocity-api:.*"))
+        }
     }
     
     build {
